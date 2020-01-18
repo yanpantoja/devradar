5c38b89d5a25cc1aa0ce2a32413903a5ff10f971
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-01-18 01:57:19 -0300

    finish backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://omni:omni@cluster0-x7f0l.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.use(express.json());
+app.use(routes);
 
 //Métodos HTTP: GET, POST, PUT, DELETE
 
@@ -10,10 +18,5 @@ app.use(express.json());
 // Query params: request.query (Filtros, ordenação, paginação, ...)
 // Route params: request.params (Identificar um recurso na alteração ou remoção)
 // Body: request.body (Dados para criação ou alteração de um registro)
-
-
-app.get('/', (req, res) => {
-    return res.json({message: 'hello world'});
-});
 
 app.listen(3333);
