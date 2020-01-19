ecc644ec35f772b6e235c5273b8e8c4b022d88cc
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-01-19 15:46:57 -0300

    commit front web reactjs

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Componente: Função JS que retorna conteúdo HTLM ou CSS ou proprio javascript
+// Propriedade: O mesmo que atributo no HTML, atributo style="", por exemplo
+// Estado: Informação mantida pelo componente, uma info lida e atualizada pelo proprio componente (Lembrar: Imutabilidade)
+
+// REGRA: Um componente por arquivo
+// variavel let != var 
+// desestruturação: basicamente pegar um vetor e dividir em variaveis
+
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
