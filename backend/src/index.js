caab5b153a7a748edc5806f0c72b017c30172fd8
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-01-19 23:47:18 -0300

    finish web front reactjs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omni:omni@cluster0-x7f0l.mongodb.net/week10?retr
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 // Componente: Função JS que retorna conteúdo HTLM ou CSS ou proprio javascript
 // Propriedade: O mesmo que atributo no HTML, atributo style="", por exemplo
@@ -7,11 +16,38 @@ import React from 'react';
 // REGRA: Um componente por arquivo
 // variavel let != var 
 // desestruturação: basicamente pegar um vetor e dividir em variaveis
-
-
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map( dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+        </ul>
+
+      </main>
+    </div>
   );
 }
 
