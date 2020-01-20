3412e47470659c64d617359d1dbe3d6360d91d09
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-01-20 11:59:00 -0300

    finish mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,8 +4,13 @@ import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
 
+import api from '../services/api';
+
 function Main({ navigation }) {
+  const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
+  const [techs, setTechs] = useState('');
+
   useEffect(() => {
     async function loadInitialPosition() {
       const { granted } = await requestPermissionsAsync();
@@ -29,30 +34,55 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs
+      }
+    });
+
+    console.log(response.data.devs);
+    
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region) {
+    console.log(region);
+    setCurrentRegion(region);
+  }
+
   if(!currentRegion) {
     return null;
   }
   return ( 
     <>
-      <MapView initialRegion={currentRegion} style={styles.map}> 
-        <Marker coordinate={{ latitude: -20.2628435, longitude: -40.300069 }}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/41760474?s=460&v=4' }}/>
+      <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map}> 
+        {devs.map(dev => (
+          <Marker key={dev._id} coordinate={{ longitude: dev.location.coordinates[0], latitude: dev.location.coordinates[1] }}>
+            <Image style={styles.avatar} source={{ uri: dev.avatar_url }}/>
 
-          <Callout onPress={() => {
-            navigation.navigate('Profile', { github_username: 'yanpantoja' });
-          }}>
-            <View style={styles.callout}>
-              <Text style={styles.devName}>Yan Pantoja</Text>
-              <Text style={styles.devBio}>Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</Text>
-              <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-            </View>
-          </Callout>
-        </Marker>
+            <Callout onPress={() => {
+              navigation.navigate('Profile', { github_username: dev.github_username });
+            }}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
 
       <View style={styles.searchForm}>
-          <TextInput style={styles.searchInput} placeholder="Buscar devs por techs..." placeholderTextColor="#999" autoCapitalize="words" autoCorrect={false}/>
-          <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <TextInput style={styles.searchInput} placeholder="Buscar devs por techs..." 
+          placeholderTextColor="#999" autoCapitalize="words" autoCorrect={false} value={techs}
+          onChangeText={setTechs} />
+          <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
             <MaterialIcons name="my-location" size={20} color="#fff"/>
           </TouchableOpacity>
       </View>
